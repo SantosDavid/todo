ea600a26509fa02baaca215eadbc6763606d4a57
SantosDavid <daviddsantosd@gmail.com>
2019-05-16 07:49:35 -0300

    adding edit my informations

---
+++ src/AppBundle/Controller/MeControllerController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use AppBundle\Form\UserUpdateType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @Route("/me", name="me.")
+ */
+class MeControllerController extends Controller
+{
+    /**
+     * @Route("edit", name="edit")
+     *
+     * @param UserInterface $user
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     *
+     * @return Response
+     */
+    public function editAction(UserInterface $user, Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $editForm = $this->createForm(UserUpdateType::class, $user);
+
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $password =  $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $this->getDoctrine()
+                ->getRepository(User::class)
+                ->update();
+        }
+
+        return $this->render(
+            'me/edit.html.twig',
+            [
+                'user' => $user,
+                'editForm' => $editForm->createView()
+            ]
+        );
+    }
+}
--- src/AppBundle/Repository/UserRepository.php
+++ src/AppBundle/Repository/UserRepository.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\User;
 use Doctrine\ORM\EntityRepository;
 
 /**
@@ -12,4 +13,9 @@ use Doctrine\ORM\EntityRepository;
  */
 class UserRepository extends EntityRepository
 {
+    public function update()
+    {
+        $em = $this->getEntityManager();
+        $em->flush();
+    }
 }
